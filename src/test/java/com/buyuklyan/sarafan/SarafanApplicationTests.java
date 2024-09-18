40608998de24c569562c924ea7f9f77fae5a5b74
MixailBuyuklyan <buyuklyan03@yandex.ru>
2024-09-18 17:29:53 +0300

    laba 1 web

---
+++ src/test/java/com/buyuklyan/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyuklyan.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
